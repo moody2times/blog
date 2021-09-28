f3cfee24874e6db48638d2b27f5e893d86e3a26b
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-28 21:02:05 +0100

    :construction: Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const ejs = require("ejs");
 // const bodyParser = require("body-parser");
+const appController = require("./controller/app.js");
 
 const app = express();
 
 app.set("views engine", "ejs");
-app.use(express.static("/pulic"));
+app.use(express.static(__dirname + "/pulic"));
+
+appController(app);
 
 app.listen(5000, () => {
 	console.log("Server is running at port 5000");
