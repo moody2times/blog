af23ac2270db0fd3efd14fec6bc9c65acd6b2782
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-28 21:02:18 +0100

    :construction: Update the app.js

--- controller/app.js
+++ controller/app.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const http = require("http");
 
 module.exports = {
-	controller: function (app) {
+	appController: function (app) {
 		app.get("/", (req, res) => {
 			//handle request and response
 		});
