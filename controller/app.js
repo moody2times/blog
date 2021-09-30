e657415d12fde0bd8a0c11430be31a17db5c6cf1
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-30 21:05:44 +0100

    :recycle: Refactor app.js

--- controller/app.js
+++ controller/app.js
@@ -1,22 +1,23 @@
-module.exports = {
-	blogController: function (app) {
-		app.get("/", (req, res) => {
-			//handle request and response
-			res.render("home");
-		});
-	},
-
-	dbClient: async function (client) {
-		const dbName = "blogDB";
-		try {
-			await client.connect();
-			const db = client.db(dbName);
-			const posts = db.collection("posts");
-			await posts.insert({ author: "Moody Mudiaga" });
-		} catch (err) {
-			console.log(err.message);
-		} finally {
-			client.close();
-		}
-	},
+const blogController = (app) => {
+	app.get("/", (req, res) => {
+		//handle request and response
+		res.render("home");
+	});
 };
+
+const dbClient = async (client) => {
+	const dbName = "blogDB";
+	try {
+		await client.connect();
+		const db = client.db(dbName);
+		const posts = db.collection("posts");
+		await posts.insertOne({ author: "Naomi Mudiaga" });
+		console.log("insert document successful!");
+	} catch (err) {
+		console.log(err.message);
+	} finally {
+		client.close();
+	}
+};
+
+module.exports = { blogController, dbClient };
