54267611baaad9b16427e570f32a371cfc492acf
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-30 20:48:49 +0100

    :construction: Update app.js

--- controller/app.js
+++ controller/app.js
@@ -1,11 +1,22 @@
-const fs = require("fs");
-const http = require("http");
-
 module.exports = {
 	blogController: function (app) {
 		app.get("/", (req, res) => {
 			//handle request and response
 			res.render("home");
 		});
+	},
+
+	dbClient: async function (client) {
+		const dbName = "blogDB";
+		try {
+			await client.connect();
+			const db = client.db(dbName);
+			const posts = db.collection("posts");
+			await posts.insert({ author: "Moody Mudiaga" });
+		} catch (err) {
+			console.log(err.message);
+		} finally {
+			client.close();
+		}
 	},
 };
