ed768d9924ae3474e677260701423f56a3ee3c19
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-30 20:37:19 +0100

    :recycle: Refactor server.js

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const { MongoClient } = require("mongodb");
 const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
 
-const controller = require("./controller/app.js").blogController;
+const { blogController, dbClient } = require("./controller/app.js");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/pulic"));
 
-controller(app);
+blogController(app);
+dbClient(client);
 
 app.listen(5000, () => {
 	console.log("Server is running at port 5000");
