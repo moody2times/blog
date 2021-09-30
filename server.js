e7b66da320cab041f8fb2a7b0a34e2796fef51ee
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-30 20:25:22 +0100

    :recycle: Refactor server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
-const controller = require("./controller/app.js");
+const controller = require("./controller/app.js").blogController;
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/pulic"));
 
-controller.blogController(app);
+controller(app);
 
 app.listen(5000, () => {
 	console.log("Server is running at port 5000");
