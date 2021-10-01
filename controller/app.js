ad2f4b8db3551dc506a15bf139a15fa8da3ab04d
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-01 13:51:56 +0100

    :construction: Update app.js

--- controller/app.js
+++ controller/app.js
@@ -7,6 +7,30 @@ const blogController = (app) => {
 		//handle request and response
 		res.render("home");
 	});
+
+	app.post("/blog", (req, res) => {
+		const _id = uniqid();
+		const { name, skill } = req.body;
+
+		const document = {
+			_id,
+			name,
+			skill,
+			age,
+		};
+
+		posts.insertOne(document, (err, result) => {
+			if (err) {
+				console.log(err.message);
+				return;
+			}
+			res.send(
+				`<h4>
+					Inserted one document with ${result.insertedId} successfully
+				</h4>`
+			);
+		});
+	});
 };
 
 module.exports = { blogController };
