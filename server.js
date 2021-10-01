49a84a3fd09039e2613b1b9a31cc3bc60a4697a3
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-01 10:25:23 +0100

    :construction: Update the server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require("express");
-// const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
-
-const url = "mongodb://localhost:27017";
-const client = new MongoClient(url);
 
 const { blogController, dbClient } = require("./controller/app.js");
 
 const app = express();
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
 
 app.set("view engine", "ejs");
+app.use(express.json());
 app.use(express.static(__dirname + "/pulic"));
 
 blogController(app);
