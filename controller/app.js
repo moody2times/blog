4442399b763060dbe7aafa4fc00fa95553a51a24
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-01 12:01:42 +0100

    :construction: Update app.js

--- controller/app.js
+++ controller/app.js
@@ -1,3 +1,7 @@
+const uniqid = require("uniqid");
+const Joi = require("joi");
+const { posts } = require("../db/db");
+
 const blogController = (app) => {
 	app.get("/", (req, res) => {
 		//handle request and response
