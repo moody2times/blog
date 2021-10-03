92e9dc198e956c060c032226ff0a765d1afc9cb6
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-03 20:08:34 +0100

    :construction: Update app.js

--- controller/app.js
+++ controller/app.js
@@ -17,34 +17,33 @@ const blogController = (app) => {
 
 	app.post("/blog", (req, res) => {
 		const _id = uniqid();
-		const { name, skill, age } = req.body;
+		const { name, age, isDeveloper, isMarried, kids } = req.body;
 
 		const document = {
 			_id,
 			name,
-			skill,
 			age,
+			isDeveloper,
+			isMarried,
+			kids,
 		};
 
-		const { value: post, error } = schema.validate(document);
-		if (error) {
-			const {
-				details: [{ message }],
-			} = error;
-			res.send(message);
-			return;
-		}
+		// const { value: post, error } = schema.validate(document);
+		// if (error) {
+		// 	const {
+		// 		details: [{ message }],
+		// 	} = error;
+		// 	res.send(message);
+		// 	return;
+		// }
 
-		posts.insertOne(post, (err, result) => {
+		posts.insertOne(document, (err, result) => {
 			if (err) {
 				console.log(err.message);
 				return;
 			}
-			res.send(
-				`<h4>
-					Inserted one document with ${result.insertedId} successfully
-				</h4>`
-			);
+			res.status(200);
+			res.json(document);
 		});
 	});
 
